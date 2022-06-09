4bd49192b02f9dec240f2833d165c6ab19a69157
YuriCJardim <yuricjardim@gmail.com>
2022-06-09 00:24:28 -0300

    Bad solution

--- src/model/enteties/Reservation.java
+++ src/model/enteties/Reservation.java
@@ -15,7 +15,7 @@ public class Reservation {
 		this.rommNumber = rommNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-			}
+	}
 
 	public Integer getRommNumber() {
 		return rommNumber;
@@ -38,23 +38,26 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		} 
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date ";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 
 	}
 
 	@Override
 	public String toString() {
-		
-		return "Reservation: Room "
-		+ getRommNumber() 
-		+  " check-in: " 
-		+ sdf.format(checkIn) 
-		+", check-out: "  +  sdf.format(checkOut) 
-		+ ", " 
-		+ duration()
-		+ " nigths";
+
+		return "Room " + getRommNumber() + " check-in: " + sdf.format(checkIn) + ", check-out: "
+				+ sdf.format(checkOut) + ", " + duration() + " nigths";
 	}
 
 }
